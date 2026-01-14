7d300ec286b03251ca5d559995226ee996dbfe42
Me <skolskymailherencsar@gmail.com>
2026-01-14 22:50:16 +0100

    Navbar Edits

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 import Navbar from './components/Navbar.vue'
 
 export default {
@@ -14,22 +13,11 @@ export default {
 <template>
   <div id = "app">
     <Navbar />
-    <!--<img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height=" 125" />-->
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-      <nav>
-        <RouterLink to="/">Home1appvue</RouterLink>
-        <RouterLink to="/reader">About2</RouterLink>
-        <RouterLink to="/library">About3</RouterLink>
-        <RouterLink to="/cart">About4</RouterLink>
-        <RouterLink to="/viewer">About5</RouterLink>
-        <RouterLink to="/about">About6</RouterLink>
-        <RouterLink to="/about">About7appvue</RouterLink>
-      </nav>
-      </div>
+    <main class="main-content">
+      <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height=" 125" />-->
       <RouterView />
-    </div>
+    </main>
+  </div>
 </template>
 
 <style>
@@ -41,10 +29,11 @@ export default {
 
 body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: whitesmoke;
+  /*margin-top: 60px;*/
+}
+
+.main-content {
+  padding-top: 80px;
 }
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,9 +2,8 @@
 
     <nav class="navbar">
         <div class="navbar-container">
-            <div class="navbar-brand">
-            <router-link to="/" class="brand-link">Temporary1</router-link>
-        </div>
+            <!--< class="navbar-brand"> -->
+            <!-- <router-link to="/" class="brand-link">Temporary1</router-link> -->
         <!-- Navlinks-->
          <div class="nav-links">
             <router-link to="/about" class="nav-link">About</router-link>
@@ -14,6 +13,7 @@
             <router-link to="/services" class="nav-link">Services</router-link>
             <router-link to="/contact" class="nav-link">Contact</router-link>
             <router-link to="/profile" class="nav-link">Profil</router-link>
+         <!-- </div> -->
          </div>
     </div>
     </nav>
@@ -28,21 +28,23 @@ export default {
     <style scoped>
 
 .navbar {
-    background-color: #333;
-    color: #fff;
-    padding: 1rem;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+    background-color: gray;
+    color: white;
     position: fixed;
     width: 100%;
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
     top: 0;
     z-index: 1000;
     height: 60px;
 }
 
 .navbar-container {
+    max-width: 800px;
+    margin: auto;
     display: flex;
-    align-items: top;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 20px;
+    height: 100%;
 }
 </style>
