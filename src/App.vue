0be6b51c025d686d4aafec3bea4f4eb563194632
Me <skolskymailherencsar@gmail.com>
2026-01-14 22:04:55 +0100

    Navbar/Styling

--- src/App.vue
+++ src/App.vue
@@ -13,28 +13,38 @@ export default {
 
 <template>
   <div id = "app">
-    <Navbar></Navbar>
-  </div>
-  <header>
-    <!--<img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />-->
+    <Navbar />
+    <!--<img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height=" 125" />-->
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/reader">About</RouterLink>
-        <RouterLink to="/library">About</RouterLink>
-        <RouterLink to="/cart">About</RouterLink>
-        <RouterLink to="/viewer">About</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">Home1appvue</RouterLink>
+        <RouterLink to="/reader">About2</RouterLink>
+        <RouterLink to="/library">About3</RouterLink>
+        <RouterLink to="/cart">About4</RouterLink>
+        <RouterLink to="/viewer">About5</RouterLink>
+        <RouterLink to="/about">About6</RouterLink>
+        <RouterLink to="/about">About7appvue</RouterLink>
       </nav>
+      </div>
+      <RouterView />
     </div>
-  </header>
-
-  <RouterView />
 </template>
 
-<style scoped>
+<style>
+  * {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
