388e4895a51b9a05fda00e89cd8371633c899df8
Me <skolskymailherencsar@gmail.com>
2026-01-17 15:11:55 +0100

    Footer loading

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,19 @@
     <main class="main-content">
       <RouterView />
     </main>
+    <Footer />
   </div>
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
     Navbar,
+    Footer,
   },
 }
 </script>
